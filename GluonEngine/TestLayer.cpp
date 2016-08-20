2c1ef51225c44cd3b34c8d2aa85dde1abc1c0b6c
ArkayCZ <vesel.fil@gmail.com>
2016-08-20 10:50:37 +0200

    Removed some old code.

--- GluonEngine/TestLayer.cpp
+++ GluonEngine/TestLayer.cpp
@@ -23,14 +23,7 @@ void TestLayer::OnInit(ge::InitBundle* bundle)
 
 	unsigned int indices[] = { 0,1,4,1,3,4,3,4,2,2,4,0,0,2,3,1,0,3 };
 	mesh = m_Renderer->GetRenderDevice()->CreateMesh(new ge::graphics::IndexedModel(vertices, 5, indices, 18));
-	shader = m_Renderer->GetRenderDevice()->CompileShader(
-		ge::files::FileUtils::LoadFile("Resources/Shaders/Basic/vertex.glsl"),
-		ge::files::FileUtils::LoadFile("Resources/Shaders/Basic/fragment.glsl")
-	);
-
 	transform = new ge::graphics::Transform(glm::vec3(0, 0, 1), glm::vec3(0), glm::vec3(1));
-	camera = new ge::graphics::Camera(glm::vec3(0, 0, -1), 70.0f, 1024.0f / 576.0f, 0.1f, 1000.0f);
-
 	ge::Entity* entity = new ge::Entity();
 	entity->AddComponent(new ge::components::RenderComponent(mesh));
 	entity->AddComponent(new ge::components::SpinningComponent());
@@ -47,14 +40,6 @@ void TestLayer::OnUpdate(ge::UpdateBundle* bundle)
 void TestLayer::OnRender()
 {
 	Layer::OnRender();
-
-	/*shader->Bind();
-	shader->SetUniform("u_WorldMatrix", transform->GetWorldMatrix(camera->GetViewProjection()));
-
-	mesh->Draw();
-
-	shader->Unbind();
-	transform->GetRotation().y += 1.0f;*/
 }
 
 void TestLayer::OnDestroy()
